2b56f7640bc7546815a9598b17389aaea724d9d5
Rohit Parakh <62591195+rohcodes4@users.noreply.github.com>
2025-10-03 04:09:42 +0530

    Added https://invoicing-software-fyhc.onrender.com to cors

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ const MONGODB_URI = 'mongodb+srv://rohitparakh4:Valorant4@cluster0.tq5zete.mongo
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: ["https://invoicing-software-frontend.onrender.com","http://localhost:3000"],
+  origin: ["https://invoicing-software-frontend.onrender.com","http://localhost:3000","https://invoicing-software-fyhc.onrender.com"],
   default:"https://invoicing-software-frontend.onrender.com"
 }
 
